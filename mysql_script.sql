8f5277fbe4a54928d42d63facd9041aa891634bf
Juan Melo <juan.melo@qualifacts.com>
2018-04-15 01:29:08 -0500

    Add DDL

---
+++ mysql_script.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `go_kafka_log` (
+  `go_kafka_log_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `txn_id` bigint(20) DEFAULT NULL,
+  `msg_id` bigint(20) DEFAULT NULL,
+  `schema_name` varchar(256) DEFAULT NULL,
+  `table_name` varchar(256) DEFAULT NULL,
+  `operation` varchar(16) DEFAULT NULL,
+  `payload` text,
+  `commit_time` timestamp NULL DEFAULT NULL,
+  `created_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`go_kafka_log_id`)
+)
